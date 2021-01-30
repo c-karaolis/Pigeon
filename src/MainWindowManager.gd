d44ea33143bb3231d3d389ebc93836e388799f65
Constantinos Karaolis <kostaskaraolis@gmail.com>
2021-01-30 11:27:07 +0200

    Connected events, minor refactor, gui changes

--- src/MainWindowManager.gd
+++ src/MainWindowManager.gd
@@ -5,46 +5,68 @@ onready var body_text_box: TextEdit = $BodyText
 onready var result_text_box: TextEdit = $ResultText
 onready var headers_text_box: TextEdit = $HeadersText
 onready var post_request: HTTPRequest = $PostHTTPRequest
+onready var get_request: HTTPRequest = $GetHTTPRequest
+onready var put_request: HTTPRequest = $PutHTTPRequest
 
 func _ready():
 	pass # Replace with function body.
 
 
-func _on_PostHTTPRequest_request_completed(result: int, response_code: int, headers: PoolStringArray, body: PoolByteArray):
+func _on_PostHTTPRequest_request_completed(_result: int, _response_code: int, _headers: PoolStringArray, _body: PoolByteArray):
 #	var json = JSON.parse(body.get_string_from_utf8())
 #	var texter = json.error if json.error != OK else json.result
-	result_text_box.text = body.get_string_from_utf8()
-	pass # Replace with function body.
+	result_text_box.text = _body.get_string_from_utf8()
 
 
-func _on_GetHTTPRequest_request_completed(result: int, response_code: int, headers: PoolStringArray, body: PoolByteArray):
-	pass
+func _on_GetHTTPRequest_request_completed(_result: int, _response_code: int, _headers: PoolStringArray, _body: PoolByteArray):
+	result_text_box.text = _body.get_string_from_utf8()
 
 
-func _on_PutHTTPRequest_request_completed(result: int, response_code: int, headers: PoolStringArray, body: PoolByteArray):
+func _on_PutHTTPRequest_request_completed(_result: int, _response_code: int, _headers: PoolStringArray, _body: PoolByteArray):
+	result_text_box.text = _body.get_string_from_utf8()
 	pass 
 
 
 func _on_PostButton_button_up():
-	var body_text: String = body_text_box.text
-	var headers: Array = headers_text_box.text.split("|")
+	var user_input: Dictionary = get_refreshed_textbox_values()
+	var body_text: String = user_input.RequestBody
 	set_result_requesting_text()
-	print (headers)
 #	var headers = ["Content-Type: application/json; charset=UTF-8"]
 	body_text = remove_json_trailing_artifacts(body_text)
-	post_request.request(url_box.text, headers, false, HTTPClient.METHOD_POST, body_text)
-	pass 
+	post_request.request(user_input.URL, user_input.Headers, false, HTTPClient.METHOD_POST, body_text)
 
 
-func remove_json_trailing_artifacts(json: String):
+func _on_GetButton_button_up():
+	
+	get_request.request(url_box.text)
+
+
+func _on_PutButton_button_up():
+	pass # Replace with function body.
+
+
+func remove_json_trailing_artifacts(json: String) -> String:
 	if json[json.length() -2] != '"':
 		json.erase(json.length() -2 , 1)
 		json = remove_json_trailing_artifacts(json)
 		return json
 	else:
 		return json
-	
+
+
+func get_refreshed_textbox_values()->Dictionary:
+	var _url: String = url_box.text
+	var _headersRaw: String = headers_text_box.text
+	var _headers: Array = _headersRaw.split("|")
+	var _bodyRaw: String = body_text_box.text
+	var _returnValues: Dictionary = {
+		"URL":_url,
+		"Headers":_headers,
+		"RequestBody":_bodyRaw
+	}
+	return _returnValues
 
 
 func set_result_requesting_text():
 	result_text_box.text = "Requesting . . ."
+
